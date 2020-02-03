769970476acbc4d5ba7197baaa2f7356f563fdae
Edewor Onyedika <edwordkaso100@gmail.com>
2020-02-03 19:55:15 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/public/';
+app.use(express.static('public'));
+
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+router.get("/individual",function(req,res){
+  res.sendFile(path + "individual.html");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
