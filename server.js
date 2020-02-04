7b2a83c39a703898f3706bdc4f6c39185d503ec6
Edewor Onyedika <edwordkaso100@gmail.com>
2020-02-04 06:24:42 +0100

    heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000;
 var express = require("express");
 var app = express();
 var router = express.Router();
@@ -8,6 +8,11 @@ app.use(express.static('public'));
 
 
 const server = http.createServer((req, res) => {
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('index.html');
+});
 
 router.get("/",function(req,res){
   res.sendFile(path + "index.html");
@@ -29,8 +34,6 @@ app.use("/",router);
 
 app.use("*",function(req,res){
   res.sendFile(path + "404.html");
-});
-
 });
 
 server.listen(port,() => {
